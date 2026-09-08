d6d7b430bf6d50061bff1625ec14bf11c701b63b
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2026-09-07 18:46:33 -0700

    Update experience details

--- src/app/experience.tsx
+++ src/app/experience.tsx
@@ -16,17 +16,17 @@ export default function Experience() {
 
     const experiences: ExperienceItem[] = [
         {
-            company: "Amazon",
-            role: "Software Development Engineering Intern (Incoming)",
-            description: "AWS Internal Tools (Fall 2026)",
+            company: "Amazon Web Services (AWS)",
+            role: "Software Development Engineering Intern",
+            description: "Task Management & Issues (Fall 2026)",
             period: "Sep 2026 - Dec 2026",
             technologies: ["Java", "Python", "JavaScript"],
             link: "https://aws.amazon.com/"
         },
         {
-            company: "Google",
+            company: "Google (YouTube)",
             role: "Software Engineering Intern",
-            description: "YouTube Android Media Player, AV1 (Summer 2026)",
+            description: "Android Media Player, AV1 (Summer 2026)",
             period: "Jun 2026 - Sep 2026",
             technologies: ["Java", "Android", "C++"],
             link: "https://www.youtube.com/"
